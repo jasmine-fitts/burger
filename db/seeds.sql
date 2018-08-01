105366a356d1071161c85c7ceb27f1553a6ce982
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-07-31 20:12:29 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured)
+
+VALUES ("Smokehouse Burger", false),
+  ("All American Burger", false),
+  ("Bacon Cheeseburger on Steriods", false),
+  ("Guacamole Burger", false),
+  ("Sunrise Burger", true);
