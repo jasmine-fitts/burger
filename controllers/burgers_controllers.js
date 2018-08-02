6839f2e0390c400e99a7c1f3e3aa12bdbe2ac7a4
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-08-01 22:40:41 -0400

    updated controllers

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+//Imports the model (burger.js) to use its database functions
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var burgerObject = {
+            burgers: data
+        };
+        console.log(burgerObject);
+        res.render("index", burgerObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id =" + req.params.id;
+    console.log("condition", condition);
+
+    bruger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        }else {
+            res.status(200).end();
+        }
+     });
+});
+
+module.exports = router;
